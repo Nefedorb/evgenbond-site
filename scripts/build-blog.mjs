df811d256f9af10b5793a27d170a959d54ba432d
Nefedorb <nefedorb@gmail.com>
2026-06-02 11:41:16 +0300

    Switch site typography to Inter Tight

--- scripts/build-blog.mjs
+++ scripts/build-blog.mjs
@@ -948,7 +948,7 @@ function renderHead({ title, description, path: pagePath, image, imageAlt, type 
     <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@200;400;700;900&family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet">`;
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@200;400;700;900&family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet">`;
 }
 
 function renderPage({ head, style, body }) {
